111deb9505aae1ec7d20a3e3eca3403b3f273786
anmol-tandon <anmoltandon.anmol123@gmail.com>
2021-04-21 01:41:45 +0530

    Concepts of 20/04/2021

---
+++ 20_04_2021.py
@@ -0,0 +1,14 @@
+# 20/04/2021
+print("Hello"+"everyone")  # String concatenation
+print('2'+'2')
+
+print("Hey"*3)  # Multiplying string with integer, here output will be HeyHeyHey
+
+x=input()  # Taking user input
+print(x)
+y=float(input("Enter your age"))  # Typecasting
+print(y)
+
+print(str(y))  # Converting inter into string
+
+# x+=3 is same as x=x+3
