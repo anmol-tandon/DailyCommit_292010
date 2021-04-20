a04ce94f282e051b642c4da63517427704cbd071
anmol-tandon <anmoltandon.anmol123@gmail.com>
2021-04-21 01:33:37 +0530

    Renaming File and adding concepts

---
+++ 19_04_2021.py
@@ -0,0 +1,19 @@
+
+# 19/04/2021
+print(10/2)  # it will give output in float
+print(9**(1/2))  # It simply means to give outpout of square root of 9 in float
+print(8**(1/3))  # To find cube root of 8
+print(20//6)  # To find quotient when 20 is divide by 6
+print(20%6)  # To find remainder
+# \n represents new line and \t represents tab
+# Can use 3 quotes to print string in multiple lines without \n again and again. For example
+print("""This 
+is
+a
+table""")
+
+
+
+
+
+
--- concepts.py
+++
@@ -1,4 +0,0 @@
-
-
-print(10/2)  # it will give output in float
-print(9**(1/2))  # It simply means to give outpout of square root of 9 in float
