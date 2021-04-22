998076f78eb2ec7e27a0452dc57f37c3a572bd92
anmol-tandon <anmoltandon.anmol123@gmail.com>
2021-04-22 19:03:33 +0530

    Basic input output and mathematical operations

--- 19_04_2021.py
+++ 19_04_2021.py
@@ -1,5 +1,8 @@
 
 # 19/04/2021
+x=input()
+print(x)
+
 print(10/2)  # it will give output in float
 print(9**(1/2))  # It simply means to give outpout of square root of 9 in float
 print(8**(1/3))  # To find cube root of 8
